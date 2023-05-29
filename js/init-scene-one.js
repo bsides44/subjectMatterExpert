7705231c85915dade7f287b1f11a51ed68110c30
bsides44 <maddog.star@gmail.com>
2023-05-29 13:47:25 +1200

    rm timeout

--- js/init-scene-one.js
+++ js/init-scene-one.js
@@ -1,31 +1,31 @@
 AFRAME.registerComponent('init-scene-one', {
 	schema: {
-	  	loaded: {type: 'boolean', default: false}
+		loaded: { type: 'boolean', default: false }
 	},
 	init: function () {
 		this.onClick = this.onClick.bind(this);
 		this.switchToMobileUI = this.switchToMobileUI.bind(this);
-		
+
 		var videoPlayer = document.querySelector("#videoPlayer").getAttribute('material').src;
 		videoPlayer.pause()
 
 		// Check for mobile
 		if (AFRAME.utils.device.isMobile()) this.switchToMobileUI()
-  
+
 		// Establish listeners
 		window.addEventListener('click', this.onClick);
 		window.addEventListener('abuttondown', this.onClick);
 		window.addEventListener('triggerdown', this.onClick);
 		window.addEventListener('gripdown', this.onClick);
 		window.addEventListener('xbuttondown', this.onClick);
-	
+
 		// Set welcome loader
 		var intro = document.querySelector('#intro');
 		var welcomeCircle = document.querySelector('#welcomeCircle');
 		var welcomeText = document.querySelector('#welcomeText');
 		var loadedText = document.querySelector('#loadedText');
 		var vrText = document.querySelector('#vrText');
-		
+
 		intro.addEventListener('progress', () => {
 			var percentLoaded = 0;
 			var duration = intro.duration;
@@ -35,7 +35,7 @@ AFRAME.registerComponent('init-scene-one', {
 				percentLoaded = Math.round((buffered.end(0) / duration) * 100);
 			}
 			if (!this.data.loaded) {
-				welcomeCircle.setAttribute('geometry', 'thetaLength', ((percentLoaded/100) * 360))
+				welcomeCircle.setAttribute('geometry', 'thetaLength', ((percentLoaded / 100) * 360))
 			}
 		});
 		// video loaded
@@ -58,57 +58,57 @@ AFRAME.registerComponent('init-scene-one', {
 			console.log(e)
 		})
 
-	  setTimeout(() => {
-			if (!this.data.loaded){
-				this.data.loaded = true
-				welcomeCircle.emit('firstVideoLoaded', null, true)
-				welcomeText.object3D.visible = false;
-				if (!vrText.getAttribute('visible')) {
-					loadedText.object3D.visible = true;
-				}
-			}
-		}, 9000);
+		//   setTimeout(() => {
+		// 		if (!this.data.loaded){
+		// 			this.data.loaded = true
+		// 			welcomeCircle.emit('firstVideoLoaded', null, true)
+		// 			welcomeText.object3D.visible = false;
+		// 			if (!vrText.getAttribute('visible')) {
+		// 				loadedText.object3D.visible = true;
+		// 			}
+		// 		}
+		// 	}, 9000);
 	},
 	onClick: function () {
-		if (this.data.loaded){
+		if (this.data.loaded) {
 			var videoPlayer = document.querySelector("#videoPlayer").getAttribute('material').src;
 			var welcomeCircle = document.querySelector('#welcomeCircle');
 			var loadedText = document.querySelector('#loadedText');
 			var vrText = document.querySelector('#vrText');
-	
+
 			// Welcome text to disappear on first click
 			if (welcomeCircle) welcomeCircle.parentNode.removeChild(welcomeCircle);
 			if (loadedText) loadedText.parentNode.removeChild(loadedText);
 			if (vrText) vrText.parentNode.removeChild(vrText);
 
-	
+
 			// Video to play on first click
 			if (videoPlayer) {
 				var vidSource = document.querySelector("#intro")
 				vidSource.muted = false
 				vidSource.play()
-	
+
 				window.removeEventListener('click', this.onClick);
 				window.removeEventListener('abuttondown', this.onClick);
 				window.removeEventListener('triggerdown', this.onClick);
 				window.removeEventListener('gripdown', this.onClick);
 				window.removeEventListener('xbuttondown', this.onClick);
 			}
-	
+
 			// Challenge text to appear at end of intro
 			setTimeout(() => {
 				this.appearChallenge()
 			}, 5800);
-	  }
+		}
 	},
-	appearChallenge: function () {  
+	appearChallenge: function () {
 		var challengeText = document.querySelector("#challengeText")
 		var startButton = document.querySelector("#startButton")
 		var videoPlayer = document.querySelector("#videoPlayer")
-	
+
 		challengeText.setAttribute('class', "cursor-listener")
 		startButton.setAttribute('class', "cursor-listener")
-	
+
 		// Make challenge appear when video playback is at 5
 		if (videoPlayer) {
 			challengeText.object3D.visible = true;
@@ -133,13 +133,13 @@ AFRAME.registerComponent('init-scene-one', {
 		this.shrink("#bonusIconTwo", 1.2, 0.3)
 		this.shrink("#bonusIconThree", 1.2, 0.3)
 		this.shrink("#eggCount", 1.2, 0.3)
-	  },
-	  shrink(id, scale, posX, posY = 0){
+	},
+	shrink(id, scale, posX, posY = 0) {
 		var element = document.querySelector(id)
-		var oldScale =  element.getAttribute('scale')
-		var oldPosition=  element.getAttribute('position')
+		var oldScale = element.getAttribute('scale')
+		var oldPosition = element.getAttribute('position')
 
-		element.setAttribute('scale', {x: oldScale.x/scale, y: oldScale.y/scale, z: oldScale.z/scale})
-		element.setAttribute('position', {x: oldPosition.x + posX, y: oldPosition.y + posY, z: oldPosition.z})
-	  },
-  });
+		element.setAttribute('scale', { x: oldScale.x / scale, y: oldScale.y / scale, z: oldScale.z / scale })
+		element.setAttribute('position', { x: oldPosition.x + posX, y: oldPosition.y + posY, z: oldPosition.z })
+	},
+});
